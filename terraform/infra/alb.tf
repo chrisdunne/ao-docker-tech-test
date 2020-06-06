a27ecd9b978b8fa266b30f8da5ce0599f162bde3
chris <contact@chrisdunne.net>
2020-06-06 14:50:00 +0100

    add target group and alb listener

--- terraform/infra/alb.tf
+++ terraform/infra/alb.tf
@@ -5,3 +5,30 @@ resource "aws_lb" "ao-test-alb" {
   security_groups    = [aws_security_group.ao-test-sg.id]
   subnets            = [aws_subnet.ao-test-subnet-a.id, aws_subnet.ao-test-subnet-b.id, aws_subnet.ao-test-subnet-c.id]
 }
+
+resource "aws_lb_listener" "ao-test-alb-listener-http" {
+  load_balancer_arn = aws_lb.ao-test-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ao-test-alb-tg-http.arn
+  }
+}
+
+#resource "aws_lb_listener" "ao-test-alb-listener-https" {
+#  load_balancer_arn = aws_lb.ao-test-alb.arn
+#  port              = "443"
+#  protocol          = "HTTPS"
+#
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.ao-test-alb-tg-https.arn
+#  }
+#}
+#
+#resource "aws_lb_listener_certificate" "ao-test-alb-listener-cert" {
+#  listener_arn    = aws_lb_listener.ao-test-alb-listener-https.arn
+#  certificate_arn = ""
+#}
---
+++ terraform/infra/tg.tf
@@ -0,0 +1,13 @@
+resource "aws_lb_target_group" "ao-test-alb-tg-http" {
+  name     = "ao-test-alb-tg-http"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ao-test-vpc.id
+}
+
+#resource "aws_lb_target_group" "ao-test-alb-tg-https" {
+#  name     = "ao-test-alb-tg-https"
+#  port     = 443
+#  protocol = "HTTPS"
+#  vpc_id   = aws_vpc.ao-test-vpc.id
+#}
