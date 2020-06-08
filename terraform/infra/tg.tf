0f72627277c4b1861c03b70ea3955cfeb9a696da
chris <contact@chrisdunne.net>
2020-06-08 07:00:01 +0100

    final final

--- terraform/infra/tg.tf
+++ terraform/infra/tg.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "ao-test-alb-tg-http" {
   name     = "ao-test-alb-tg-http"
-  port     = 80
+  port     = 32768
   protocol = "HTTP"
   vpc_id   = aws_vpc.ao-test-vpc.id
 }
